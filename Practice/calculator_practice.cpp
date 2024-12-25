da4d2f6bb69e334ed3715454838dcbe23f9a4728
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2024-12-25 23:48:46 +0500

    Calculator file added

---
+++ Practice/calculator_practice.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a,b = 0;
+    char op ;
+
+    cout<<"Enter 1st number : ";
+    cin>>a;
+    cout<<"Enter 2st number : ";
+    cin>>b;
+    cout<<"Enter Operator + - * / : ";
+    cin>>op;
+
+    
+}
