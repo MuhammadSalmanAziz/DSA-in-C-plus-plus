90880b651449095f2908171436c40b1e95391773
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2024-12-26 22:04:31 +0500

    Practice file for lecture 2 added

---
+++ Practice/lecture_2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   /* int a = 5.0;
+    char grade = 'a';
+    int grade_ascii = grade;
+    double b = 4.0;
+    int a_b = b;
+    double PI = 3.14f;
+    cout<<grade_ascii<<endl;
+    cout<<a_b;
+    return 0;*/
+
+// Type conversion
+    /*double price = 1000.99;
+    int new_price = price;
+    cout<<new_price;*/
+
+// Type casting
+/* double price_1= 40.888;
+ int new_price_1 = (int)price_1;
+ cout<<new_price_1;*/
+
+ // Inputing from user
+
+ /*int number; 
+ cout<<"Enter the number :";
+ cin>>number;
+ cout<<"You Enter : "<<number;*/
+
+ // Floating answer
+
+ /*int ab = 5;
+ int bc = 2;
+ cout<<ab/(double)bc;
+*/
+
+//unary operator
+
+/*int i = 5;
+int j = i++; // Post increment
+cout<<i<<endl;
+cout<<j;*/
+
+int k = 10;
+int c = ++k;
+cout<<" k is "<<k<<endl;
+cout<<"C is "<<c;
+}
