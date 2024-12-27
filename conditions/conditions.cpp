b17f18a7ab098d0a30ed6430b4567eec50fd0c59
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2024-12-27 23:57:33 +0500

    condition file added

---
+++ conditions/conditions.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*int n = 20;
+    if (n >= 0)
+    {
+        cout << "n positive\n" << endl;
+    }
+    else
+    {
+        cout << "n is negative" << endl;
+    }
+    return 0;*/
+
+    // Program to check if a person is eligible to vote or not
+    /*int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age >= 18)
+    {
+        cout << "You are eligible to vote" << endl;
+    }
+    else
+    {
+        cout << "You are not eligible to vote" << endl;
+    }*/
+
+    // Program to check if a number is even or odd
+    /*int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n % 2 == 0)
+    {
+        cout << "The number is even" << endl;
+    }
+    else
+    {
+        cout << "The number is odd" << endl;
+    }*/
+    // Graing system
+
+    /*int marks;
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    if (marks >= 90)
+    {
+        cout << "Grade A" << endl;
+    }
+    else if (marks >= 80 && marks < 90)
+    {
+        cout << "Grade B" << endl;
+    }
+    else{
+        cout << "Grade C" << endl;
+    }*/
+   //Program to check if the character is lowercase or uppercase
+   /*char ch;
+   cout << "Enter a character: ";
+   cin >> ch;  
+   if (ch >= 'a' && ch <= 'z'){
+         cout << "The character is lowercase" << endl;
+    }
+    else{
+        cout << "The character is uppercase" << endl;
+    }*/
+   // Using second method
+   /*char ch;
+   cout << "Enter a character: ";
+   cin >> ch;  
+   if (ch >= 65 && ch <= 90){
+         cout << "The character is uppercase" << endl;
+    }
+    else{
+        cout << "The character is lowercase" << endl;
+    }*/
+
+   // Ternaaary operator
+   int n = -40;
+   cout<< "The number is "<<(n >=0 ? "positive" : "negative") << endl;
+    return 0;
+
+}
