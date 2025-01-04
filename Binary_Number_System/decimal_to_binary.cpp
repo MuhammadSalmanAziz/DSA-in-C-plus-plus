6c7128422196f4ef6215cb5ec2073979d879d222
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2025-01-04 21:39:13 +0500

    binary numbver system added

---
+++ Binary_Number_System/decimal_to_binary.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int decToBinary(int deciMal){
+//Decimal to binary
+    int pow = 1;
+    int ans = 0;
+    while(deciMal > 0){
+        int remainder = deciMal %2;
+        deciMal = deciMal/2;
+        ans += (remainder*pow);
+        pow = pow * 10;
+    }
+    return ans;
+}
+
+int main(){
+    for(int i = 1;i<=20;i++){
+        cout<<decToBinary(i)<<endl;
+    }
+}
