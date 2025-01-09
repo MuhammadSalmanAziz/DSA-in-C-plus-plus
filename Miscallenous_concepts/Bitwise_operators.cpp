42367fe4ff47fffc9f7f68c76ec275da193481fd
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2025-01-09 20:55:00 +0500

    Bitwise operator file added

---
+++ Miscallenous_concepts/Bitwise_operators.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 4, b = 8;
+    // Bitwise AND Operator
+    //  cout<<"Bitwise AND of "<<a<<" and " <<b<<" is "<<(a & b)<<endl;
+    //  Bitwise OR Operator
+    //  cout<<"Bitwise OR of "<<a<<" and " <<b<<" is "<<(a | b)<<endl;
+    //  Bitwise XOR operator
+    //  cout<<"Bitwise XOR of "<<a<<" and " <<b<<" is "<<(a ^ b)<<endl;
+
+    // left shift
+    int n = 4;
+    cout<<n<<endl;
+    cout<<(n<<2)<<endl;
+    return 0;
+}
