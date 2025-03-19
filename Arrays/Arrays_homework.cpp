b92459212ce42f79dc8d65332bcc3a2db18dbfb4
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2025-03-19 22:11:07 +0500

    Practice files added

--- Arrays/Arrays_homework.cpp
+++ Arrays/Arrays_homework.cpp
@@ -57,18 +57,7 @@ void larget_smallest_num_and_indexes(){
 
 int main()
 {
-    int size = 8;
-   int arr[] = {1,2,3,4,5,6,7,8};
-   int start = 0;
-   int end = size -1;
-   while(start < end){
-    swap(arr[start],arr[end]);
-    start++;
-    end--;
-   }
-   for(int i = 0;i<size;i++){
-    cout<<arr[i];
-   }
+  
    
     return 0;
 }
