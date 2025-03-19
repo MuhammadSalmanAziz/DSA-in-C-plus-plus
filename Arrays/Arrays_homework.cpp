ca94942f21fbbd44300ad2e5b4e81414e243e1f3
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2025-03-19 22:47:50 +0500

    Intersection code added

--- Arrays/Arrays_homework.cpp
+++ Arrays/Arrays_homework.cpp
@@ -75,8 +75,20 @@ void unique_values()
         }
     }
 }
+void intersection(){
+    int arr1 []= { 1,4,6,5,8};
+    int arr2 [] = {2,8,5,6,7};  
+    int size = sizeof(arr1)/ sizeof(int);
+    for(int i = 0;i<size;i++){
+        for(int j =0;j<size;j++){
+            if(arr1[i] == arr2[j]){
+            cout<<arr1[i];
+            }
+        }
+    }
+}
 int main()
 {
-
+    
     return 0;
 }
