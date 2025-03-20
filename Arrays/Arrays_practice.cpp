6b4cd8da2c5b962dee68e25552cdcf9e750f9c2b
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2025-03-20 22:15:47 +0500

    Arrays practical file added

--- Arrays/Arrays_practice.cpp
+++ Arrays/Arrays_practice.cpp
@@ -23,6 +23,34 @@ void reverse(){
      cout<<arr[i];
     }
 }
+void swap_max_and_min(int arr[],int size){
+    int smallest = INT_MAX;
+    int largest = INT_MIN;
+    int smallest_index = 0;
+    int largest_index = 0;
+
+    for(int i = 0 ;i<size-1;i++){
+        smallest = min(arr[i],smallest);
+        if(arr[i] == smallest){
+            smallest_index = i;
+        }
+        largest = max(arr[i],largest);
+        if (arr[i]==largest)
+        {
+            /* code */
+            largest_index = i;
+        }
+    }
+    cout<<"The smallest number is "<<smallest<<endl;
+    cout<<"The smallest number index is "<<smallest_index<<endl;
+    cout<<"The largest number is "<<largest<<endl;
+    cout<<"The largest number index is "<<largest_index<<endl;
+    swap(arr[smallest_index],arr[largest_index]);
+
+    for(int j = 0 ; j<size;j++){
+        cout<<arr[j];
+    }
+}
 int main(){
 
     // int smallest = INT_MAX;
@@ -42,9 +70,10 @@ int main(){
     // cout<<"The smallest num in the list is "<<smallest<<endl;
     // cout<<"The largest num in the list is "<<largest<<endl;
     int size  = 5;
-    int target  = 40;
     int arr [size] = {3,5,4,6,7};
-    int res = linear_search(arr,size,target);
-    cout<<"The location of the target is "<<res;
-    return 0;
+    swap_max_and_min(arr,size);
+    // for(int j = 0 ; j<size;j++){
+    //     cout<<arr[j];
+    // }
+    // return 0;
 }
