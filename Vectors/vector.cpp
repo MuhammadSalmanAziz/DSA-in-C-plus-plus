4c099edbcc93a4015f892ab5a04235d82f2e70c7
MuhammadSalmanAziz <m.salmanaziz12@gmail.com>
2025-03-26 22:46:58 +0500

    Code for finding the single element in vector is added

--- Vectors/vector.cpp
+++ Vectors/vector.cpp
@@ -1,21 +1,26 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int linear_search(vector<int> vec ,int size, int target){
-    for(int i = 0; i<size;i++){
-        if(vec[i] == target){
+int linear_search(vector<int> vec, int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (vec[i] == target)
+        {
             return i;
         }
     }
     return -1;
 }
 
-void reverse_vec(vector<int>&vec){
+void reverse_vec(vector<int> &vec)
+{
     int start = 0;
-    int end = vec.size() -1;
-    while(start<end){
-        swap(vec[start],vec[end]);
+    int end = vec.size() - 1;
+    while (start < end)
+    {
+        swap(vec[start], vec[end]);
         start++;
         end--;
     }
@@ -24,17 +29,32 @@ void reverse_vec(vector<int>&vec){
     // }
 }
 
-void reverse_vec(){
-    vector<int> vec = {1,2,3};
+void reverse_vec()
+{
+    vector<int> vec = {1, 2, 3};
     // int target = 2;
     // int res = linear_search(vec,vec.size(),target);
     // cout<<res;
     reverse_vec(vec);
-    for(int val : vec){
-        cout<<val;
+    for (int val : vec)
+    {
+        cout << val;
     }
 }
-int main(){
-    
 
+int single_element(vector<int> vec)
+{
+    int ans = 0;
+    for (int val : vec)
+    {
+        ans ^= val;
+    }
+    return ans;
+}
+int main()
+{
+    vector<int>vec = {2,4,4,2,1,1,9};
+    int answer = single_element(vec);
+    cout<<"The single element in vector is "<<answer<<endl;
+    return 0;
 }
